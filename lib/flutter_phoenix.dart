8b2de670e63d5311d213e0e62750f780b391a346
minhdanh <minhdanh@users.noreply.github.com>
2024-09-05 07:43:28 +0700

    Use method from instance

--- lib/flutter_phoenix.dart
+++ lib/flutter_phoenix.dart
@@ -5,25 +5,30 @@ import 'package:flutter/widgets.dart';
 /// Wrap your root App widget in this widget and call [Phoenix.rebirth] to restart your app.
 class Phoenix extends StatefulWidget {
   final Widget child;
+  final Future<void> Function()? beforeRebirth;
 
-  Phoenix({Key? key, required this.child}) : super(key: key);
+  Phoenix({
+    Key? key,
+    required this.child,
+    this.beforeRebirth,
+  }) : super(key: key);
 
   @override
   _PhoenixState createState() => _PhoenixState();
 
-  static rebirth(BuildContext context,
-      {Future<void> Function()? beforeRebirth}) async {
-    if (beforeRebirth != null) {
-      await beforeRebirth.call();
-    }
-    context.findAncestorStateOfType<_PhoenixState>()!.restartApp();
+  static rebirth(BuildContext context) async {
+    await context.findAncestorStateOfType<_PhoenixState>()!.restartApp();
   }
 }
 
 class _PhoenixState extends State<Phoenix> {
   Key _key = UniqueKey();
 
-  void restartApp() {
+  Future<void> restartApp() async {
+    if (widget.beforeRebirth != null) {
+      await widget.beforeRebirth?.call();
+    }
+
     setState(() {
       _key = UniqueKey();
     });
